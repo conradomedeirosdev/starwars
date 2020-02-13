f238d783603b8547ffeaba96dde4554fcf7d9056
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-02-13 20:50:57 -0300

    Updates

---
+++ precache-manifest.cb949fbf98700720299519f8b667cecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4bd4cd3d90930f69b992b1180afa78",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "01c12df2db83bed89d91",
+    "url": "/starwars/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dedb08691e07057e32d",
+    "url": "/starwars/static/js/2.898af350.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/starwars/static/js/2.898af350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c12df2db83bed89d91",
+    "url": "/starwars/static/js/main.6518bc5a.chunk.js"
+  },
+  {
+    "revision": "9e497872391c0d90d85d",
+    "url": "/starwars/static/js/runtime-main.82cdc84b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starwars/static/media/logo.5d5d9eef.svg"
+  }
+]);
